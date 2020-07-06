354142ffdfa130b4fdedc5a0f6fb5ce4c9829702
franklin <franklinhyriol@gmail.com>
2020-07-06 09:30:42 +0300

    front change

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,13 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-
-
-//= require jquery/jquery-3.2.1.min
-//= require animsition/js/animsition.min
-//= require bootstrap/js/popper
-//= require bootstrap/js/bootstrap.min
-//= require select2/select2.min
-//= require daterangepicker/moment.min
-//= require daterangepicker/daterangepicker
-//= require countdowntime/countdowntime
-//= require js/main
